9618e84079f7632c1d08bfd9d9879ac0b6ad8191
cupstealer <guisilvanegrinho@gmail.com>
2023-03-10 18:03:04 +0000

    Adding the special type, all working

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -25,14 +25,23 @@ namespace Specials
             decimal max6 = decimal.MaxValue;
             decimal min6 = decimal.MinValue;
 
+            //Double Infinity
+            double x1 = double.PositiveInfinity;
+            double x2 = double.NegativeInfinity;
+            //Float NAN
+            float x = float.NaN;
 
             Console.WriteLine();
-            Console.WriteLine($"Range of Double Type is {min} to {max}");
             Console.WriteLine($"Range of Int Type is {min2} to {max2}");
             Console.WriteLine($"Range of Short Type is {min3} to {max3}");
             Console.WriteLine($"Range of Sbyte Type is {min4} to {max4}");
             Console.WriteLine($"Range of Float Type is {min5} to {max5}");
             Console.WriteLine($"Range of Decimal Type is {min6} to {max6}");
+            Console.WriteLine($"Range of Double Type is {min} to {max}");
+            
+            Console.WriteLine();
+            Console.WriteLine($"Range of Infinity Double Type is {x1} to {x2}");
+            Console.WriteLine($"The Invalid Float Type is {x}");
         }
     }
 }
