46cce7b44cfc21bc9b00106a7eb267227f06e154
cupstealer <guisilvanegrinho@gmail.com>
2023-03-10 16:20:28 +0000

    Now asking the radius

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -7,9 +7,13 @@ namespace PerguntaValores
         static void Main(string[] args)
 
         {
-            int height;
+            int height, radius;
+            float volume;
+
             Console.WriteLine("Insert the object's height: ");
             height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the object's radius: ");
+            radius = int.Parse(Console.ReadLine());
         }
     }
 }
