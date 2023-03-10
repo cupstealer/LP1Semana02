6ad26b84a7d91d1eb1ad0085a5631e25ac190203
cupstealer <guisilvanegrinho@gmail.com>
2023-03-10 19:14:19 +0000

    Copied the Grandpa File, it's already working as pretended, it seems

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question, response;
+
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            switch (question)
+            
+            {
+                case "How are you?":
+                    response = "I'm fine, thank you";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            Console.WriteLine(response);
+        }
+    }
+}
