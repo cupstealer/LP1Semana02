4784f924e579f0a9a47c7ffdb9988a7f4270a677
cupstealer <guisilvanegrinho@gmail.com>
2023-03-10 16:17:59 +0000

    Asking for height's input

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -5,14 +5,11 @@ namespace PerguntaValores
     class Program
     {
         static void Main(string[] args)
+
         {
-            
-            
-
-
-
-
-
+            int height;
+            Console.WriteLine("Insert the object's height: ");
+            height = int.Parse(Console.ReadLine());
         }
     }
 }
