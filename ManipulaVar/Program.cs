abf2325b72568ad83136c8a7af9994599e070956
cupstealer <guisilvanegrinho@gmail.com>
2023-03-10 11:45:14 +0000

    Reading and Adding +1

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+    
+        {
+        
+            string str = args[0];
+            int x = int.Parse(str);
+            int x2 = int.Parse(str);
+            x++;
+
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
