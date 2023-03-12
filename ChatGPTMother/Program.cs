fd79793a1dbe97c075a9574fbd6e858e4dd350c3
cupstealer <guisilvanegrinho@gmail.com>
2023-03-12 18:45:07 +0000

    Adding the command to break the loop

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -11,11 +11,16 @@ namespace ChatGPTMother
                 string question, response;
 
                 Console.WriteLine();
-                Console.Write("What is your question? ");
+                Console.Write("Type EXIT if you want to end this Chat. ");
+                Console.Write ("What is your question? ");
                 question = Console.ReadLine();
 
                 switch (question)
                 {
+                case "EXIT":
+                    response = "Exiting now, see you soon!";
+                    Environment.Exit(1);
+                    break;
                 case "How are you?":
                     response = "I feel amazing, thank you";
                     break;
@@ -25,13 +30,14 @@ namespace ChatGPTMother
                 case "How's your day?":
                     response = "It's been alright, but I feel better now since I have some company!";
                     break;
+                case "How old are you?":
+                    response = "Well, I don't have an age since I'm just a bot, but if I was real, maybe 45...";
+                    break;
                 default:
                     response = "I'm sorry honey, I still haven't learn that.";
                     break;
                 }
                 Console.WriteLine(response);
-
-                Environment.Exit(1);
             }
         }
     }
